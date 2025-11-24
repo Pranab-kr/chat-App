a78867d33eb392021f3f33acd94f45fdce63685e
pranab <pranabm406@gmail.com>
2025-11-24 21:09:03 +0530

    navBar set

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 import { apiClient } from "../lib/axios.js";
-
+import { toast } from "react-hot-toast";
 export const useAuthStore = create((set) => ({
   authUser: null,
   isSigningUp: false,
@@ -8,6 +8,7 @@ export const useAuthStore = create((set) => ({
   isUpdatingProfile: false,
   isCheckingAuth: true,
 
+  //check authentication status first page load
   checkAuth: async () => {
     try {
       const res = await apiClient.get("/auth/current-user");
@@ -27,6 +28,8 @@ export const useAuthStore = create((set) => ({
       await apiClient.post("/auth/signup", formData);
       // Don't set authUser here - user needs to login to get cookies
       set({ authUser: null });
+
+      toast.success("Account created! Please login");
     } catch (error) {
       console.log("error in signup", error);
       set({ authUser: null });
@@ -42,11 +45,25 @@ export const useAuthStore = create((set) => ({
       const res = await apiClient.post("/auth/login", formData);
 
       set({ authUser: res.data.user });
+
+      toast.success("Logged in successfully");
     } catch (error) {
       console.log("error in login", error);
+      toast.error("Login failed. Please check your credentials.");
       throw error;
     } finally {
       set({ isLoggingIn: false });
+    }
+  },
+  logout: async () => {
+    try {
+      await apiClient.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logged out successfully");
+    } catch (error) {
+      console.log("error in logout", error);
+      toast.error("Logout failed. Please try again.");
+      throw error;
     }
   },
 }));
